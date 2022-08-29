33be1cc3dadd086a855e22fdd7a35a108308fcaa
Joao P G Begiato <jpbegiato@hotmail.com>
2022-08-29 19:33:21 -0300

    criação da segunda rota GET /user/login, que irá selecionar o usuário por login

--- Orange-notes-API/src/controllers/UserController.js
+++ Orange-notes-API/src/controllers/UserController.js
@@ -10,5 +10,17 @@ class User{
             const response = await DatabaseUserMethod.listAllUser();
             res.status(200).json(response)
         })
+
+        app.get("/user:login", async(req, res)=>{
+            try{
+                const user = await DatabaseUserMethod.listUserByLogin(req.params.login)
+                if(!user){
+                    throw new Error("Usuário não cadastrado.")
+                }
+                res.status(200).json(user)
+            }catch(error){
+                res.status(404).json(error.message)
+            }
+        })
     }
 }
